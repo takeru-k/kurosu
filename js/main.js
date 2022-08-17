86f282ab34235bf088223b040f203930a271aee3
takeru-k <kam41893@gmail.com>
2022-08-17 10:06:34 +0900

    要望反映

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ $(function () {
 
 var ww = window.innerWidth || document.documentElement.clientWidth || 0;
 		if (ww > 767){
-			var srcBgArray = [{ src: 'images/top/mv.jpg' }, //PCでスライドする画像を配列で設定
-    				 { src: 'images/top/mv2.jpg' },
-					 { src: 'images/top/mv3.jpg' }];
+			var srcBgArray = [{ src: 'images/top/mv3.jpg' }, //PCでスライドする画像を配列で設定
+    				 { src: 'images/top/mv.jpg' },
+					 { src: 'images/top/mv2.jpg' }];
 		} else {
-			var srcBgArray = [{ src: 'images/top/mv.jpg' }, //スマホでスライドする画像を配列で設定
-    				 { src: 'images/top/mv2.jpg' },
-					 { src: 'images/top/mv3.jpg' }];
+			var srcBgArray = [{ src: 'images/top/mv3.jpg' }, //スマホでスライドする画像を配列で設定
+    				 { src: 'images/top/mv.jpg' },
+					 { src: 'images/top/mv2.jpg' }];
 			
 		}
 		$('#slider').vegas({ 
